a370fd3fcb68e4449496dfaafd0720bf4162b47c
Rawshan Ali <pust.love008@gmail.com>
2023-09-22 11:08:02 +0600

    Blog page added

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index() {
-        return view('posts.index');
+        return view('posts.index', [
+            'posts' => Post::take(3)->get(),
+        ]);
     }
 }
